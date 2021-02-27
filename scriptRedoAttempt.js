f23dad68bbe330f48c5be5813220930eb41129f3
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-02-27 00:23:48 -0600

    why so hard!??!? another backup!

--- scriptRedoAttempt.js
+++ scriptRedoAttempt.js
@@ -1,18 +1,16 @@
 var dots = document.getElementById('dots');
 var undo = document.getElementById('undo');
 var redo = document.getElementById('redo');
-
-var myHistory = [[]];
+var myHistory = [Immutable.List([])];
 var historyIndex = 0;
-
 function operation(fn) {
     myHistory = myHistory.slice(0, historyIndex + 1);
+    console.log(myHistory);
     var newVersion = fn(myHistory[historyIndex]);
     myHistory.push(newVersion);
     historyIndex++;
     draw();
 }
-
 function addDot(x, y) {
     operation(function (data) {
         return data.push({
@@ -22,35 +20,22 @@ function addDot(x, y) {
         });
     });
 }
-
 function draw() {
-    // dots.innerHTML = '';
-    // myHistory[historyIndex].forEach(function (dot) {
-    //     var elem = dots.appendChild(document.createElement('div'));
-    //     elem.className = 'dot';
-    //     elem.style.left = dot.get('x') + 'px';
-    //     elem.style.top = dot.get('y') + 'px';
-    //     elem.addEventListener('click', function (e) {
-    //         removeDot(dot.get('id'));
-    //         e.stopPropagation();
-    //     });
-    // });
+    dots.innerHTML = '';
+    myHistory[historyIndex].forEach(function (dot) {
+        var elem = dots.appendChild(document.createElement('div'));
+        elem.className = 'dot';
+        elem.style.left = dot['x'] + 'px';
+        elem.style.top = dot['y'] + 'px';
+        elem.addEventListener('click', function (e) {
+            removeDot(dot.get('id'));
+            e.stopPropagation();
+        });
+    });
     undo.disabled = (historyIndex != 0) ? '' : 'disabled';
     redo.disabled = (historyIndex !== myHistory.length - 1) ? '' : 'disabled';
 }
-
 dots.addEventListener('click', function (e) {
     addDot(e.pageX, e.pageY);
 });
-
-undo.addEventListener('click', function () {
-    if (historyIndex > 0) historyIndex--;
-    draw();
-});
-
-redo.addEventListener('click', function () {
-    if (historyIndex < myHistory.length) historyIndex++;
-    draw();
-});
-
 draw();
